a07f429e4ccd1724a2d44c4532b0540a6bf4ef04
Kaguya <rx700@vip.qq.com>
2026-06-11 22:21:33 +0800

    Added SensorsControllerV0.

---
+++ LenovoLegionToolkit.Lib/Controllers/Sensors/SensorsControllerV0.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using LenovoLegionToolkit.Lib.System.Management;
+using LenovoLegionToolkit.Lib.Utils;
+
+namespace LenovoLegionToolkit.Lib.Controllers.Sensors;
+
+public class SensorsControllerV0(GPUController gpuController) : AbstractSensorsController(gpuController)
+{
+    private const int CPU_FAN_ID = 1;
+    private const int GPU_FAN_ID = 2;
+    private const int PCH_FAN_ID = 4;
+
+    public override async Task<bool> IsSupportedAsync()
+    {
+        try
+        {
+            var result = await WMI.LenovoFanTestData.ExistsAsync(CPU_FAN_ID).ConfigureAwait(false);
+            result &= await WMI.LenovoFanTestData.ExistsAsync(GPU_FAN_ID).ConfigureAwait(false);
+
+            if (result)
+                _ = await GetDataAsync().ConfigureAwait(false);
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            Log.Instance.Trace($"Error checking support. [type={GetType().Name}]", ex);
+
+            return false;
+        }
+    }
+
+    protected override async Task<int> GetCpuCurrentTemperatureAsync()
+    {
+        return 0;
+    }
+
+    protected override async Task<int> GetGpuCurrentTemperatureAsync()
+    {
+        return 0;
+    }
+    protected override async Task<int> GetPchCurrentTemperatureAsync()
+    {
+        return 0;
+    }
+
+    protected override Task<int> GetCpuCurrentFanSpeedAsync() => WMI.LenovoOtherMethod.GetFeatureValueAsync(CapabilityID.CpuCurrentFanSpeed);
+
+    protected override Task<int> GetGpuCurrentFanSpeedAsync() => WMI.LenovoOtherMethod.GetFeatureValueAsync(CapabilityID.GpuCurrentFanSpeed);
+    protected override Task<int> GetPchCurrentFanSpeedAsync() => WMI.LenovoOtherMethod.GetFeatureValueAsync(CapabilityID.PchCurrentFanSpeed);
+
+    protected override Task<int> GetCpuMaxFanSpeedAsync() => WMI.LenovoFanTestData.GetFanMaxSpeedAsync(CPU_FAN_ID);
+
+    protected override Task<int> GetGpuMaxFanSpeedAsync() => WMI.LenovoFanTestData.GetFanMaxSpeedAsync(GPU_FAN_ID);
+    protected override Task<int> GetPchMaxFanSpeedAsync() => WMI.LenovoFanTestData.GetFanMaxSpeedAsync(PCH_FAN_ID);
+}
---
+++ LenovoLegionToolkit.Lib/System/Management/WMI.LenovoFanTestData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LenovoLegionToolkit.Lib.Extensions;
+
+// ReSharper disable InconsistentNaming
+// ReSharper disable StringLiteralTypo
+
+namespace LenovoLegionToolkit.Lib.System.Management;
+
+public static partial class WMI
+{
+    public static class LenovoFanTestData
+    {
+        public static Task<bool> ExistsAsync(int fanId) =>
+            WMI.ExistsAsync("root\\WMI", $"SELECT * FROM LENOVO_FAN_TABLE_DATA WHERE FanId = {fanId}");
+
+        public static Task<IEnumerable<(bool active, uint[] fanIds, uint[] fanMaxSpeeds, uint[] fanMinSpeeds, uint numOfFans)>> ReadAsync() =>
+                    WMI.ReadAsync("root\\WMI",
+                    $"SELECT * FROM LENOVO_FAN_TEST_DATA",
+                    pdc =>
+                    {
+                        var active = Convert.ToBoolean(pdc["Active"].Value);
+                        var numOfFans = Convert.ToUInt32(pdc["NumOfFans"].Value);
+                        var fanIds = (uint[]?)pdc["FanId"].Value ?? [];
+                        var fanMaxSpeeds = (uint[]?)pdc["FanMaxSpeed"].Value ?? [];
+                        var fanMinSpeeds = (uint[]?)pdc["FanMinSpeed"].Value ?? [];
+
+                        return (active, fanIds, fanMaxSpeeds, fanMinSpeeds, numOfFans);
+        });
+
+        public static async Task<int> GetFanMaxSpeedAsync(int fanId)
+        {
+            var results = await WMI.ReadAsync("root\\WMI",
+                $"SELECT * FROM LENOVO_FAN_TEST_DATA",
+                pdc =>
+                {
+                    var fanIds = (uint[]?)pdc["FanId"].Value ?? [];
+                    var fanMaxSpeeds = (uint[]?)pdc["FanMaxSpeed"].Value ?? [];
+
+                    var index = Array.IndexOf(fanIds, (uint)fanId);
+
+                    if (index >= 0 && index < fanMaxSpeeds.Length)
+                    {
+                        return (int)fanMaxSpeeds[index];
+                    }
+
+                    return -1;
+                }).ConfigureAwait(false);
+
+            return results.FirstOrDefault(speed => speed > -1);
+        }
+    }
+}
